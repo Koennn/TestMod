bea00c35b3bb1c2192480f90a98e4e982d00cf83
Koennn <koenwillemse1@gmail.com>
2016-01-24 21:01:46 +0100

    Tried to fix texture

--- src/main/java/me/koenn/mod/reference/ItemReference.java
+++ src/main/java/me/koenn/mod/reference/ItemReference.java
@@ -2,5 +2,5 @@ package me.koenn.mod.reference;
 
 public class ItemReference {
 
-    public static final String MAGIC_WAND_NAME = "magic_wand";
+    public static final String MAGIC_WAND_NAME = GeneralReference.MOD_ID + ":magic_wand";
 }
